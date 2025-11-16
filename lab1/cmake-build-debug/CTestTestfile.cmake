19874a94ffd9b76ed2a8b60ba611e522f15a0198
Denis Dunda <d.dunda@g.nsu.ru>
2025-11-16 23:24:18 +0700

    with_test_and_rework

---
+++ lab1/cmake-build-debug/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Admin/Desktop/labs/OOP_labs/OOP_labs/lab1
+# Build directory: C:/Users/Admin/Desktop/labs/OOP_labs/OOP_labs/lab1/cmake-build-debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test([=[BitArrayTests]=] "C:/Users/Admin/Desktop/labs/OOP_labs/OOP_labs/lab1/cmake-build-debug/test_bitarray.exe")
+set_tests_properties([=[BitArrayTests]=] PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Admin/Desktop/labs/OOP_labs/OOP_labs/lab1/CMakeLists.txt;45;add_test;C:/Users/Admin/Desktop/labs/OOP_labs/OOP_labs/lab1/CMakeLists.txt;0;")
+subdirs("_deps/googletest-build")

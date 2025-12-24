ea768a5c1aadca967568193fe9631c305a20bf88
Denis Dunda <d.dunda@g.nsu.ru>
2025-12-24 18:54:53 +0700

    rm build

--- lab0/0b/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/denis/OOP_labs/lab0/0b
-# Build directory: /home/denis/OOP_labs/lab0/0b/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[test_lab]=] "/home/denis/OOP_labs/lab0/0b/cmake-build-debug/run_tests")
-set_tests_properties([=[test_lab]=] PROPERTIES  _BACKTRACE_TRIPLES "/home/denis/OOP_labs/lab0/0b/CMakeLists.txt;32;add_test;/home/denis/OOP_labs/lab0/0b/CMakeLists.txt;0;")
